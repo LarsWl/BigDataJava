7760814bbfd832c80eb3f0c8334a6637942b5110
Egor Shmorgun <egorshmorgun@yandex.ru>
2023-03-02 22:18:46 +0300

    update var2_task4;

--- lab1/var2_task4/src/Main.java
+++ lab1/var2_task4/src/Main.java
@@ -1,19 +1,33 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Source args list: %s \n", Arrays.toString(args));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter N:");
+        int n = in.nextInt();
 
-        List<Integer> suitable_args_list = Stream.
-                of(args)
-                .map(Integer::valueOf)
+        List<Integer> numbers = new ArrayList<>();
+
+        System.out.printf("Enter %d numbers:\n", n);
+
+        for(int i = 0; i < n; i = i + 1) {
+            Integer number = in.nextInt();
+
+            numbers.add(number);
+        }
+        System.out.printf("Source numbers list: %s \n", numbers);
+
+        List<Integer> suitable_numbers = numbers
+                .stream()
                 .filter(arg -> (arg % 5 == 0) && (arg % 7 == 0))
                 .collect(Collectors.toList());
 
 
-        System.out.printf("Filtered args list: %s \n", suitable_args_list);
+        System.out.printf("Filtered numbers list: %s \n", suitable_numbers);
     }
 }
